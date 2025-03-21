6a96a51b1e98da9721dfcf0b008f36dcb5670199
Joel D <joeldasari10@gmail.com>
2025-03-21 09:18:52 +0530

    Updated DeviceStats and LocationStats

--- src/components/LocationStats.jsx
+++ src/components/LocationStats.jsx
@@ -25,18 +25,20 @@ export default function LocationStats({ stats }) {
   }));
 
   return (
-    <ResponsiveContainer width={"100%"} height={300}>
-      <LineChart
-        data={cities.slice(0, 5)}
-        margin={{ top: 20 }}
-        accessibilityLayer
-      >
-        <XAxis dataKey="City" padding={{ left: 30, right: 30 }} />
-        <YAxis tickCount={1} />
-        <Tooltip labelStyle={{ color: "black" }} />
-        <Legend />
-        <Line type="monotone" dataKey="Count" stroke="#3B82F6"></Line>
-      </LineChart>
-    </ResponsiveContainer>
+    <div className="h-[200px] md:h-[250px] xl:h-[300px] w-full m-auto">
+      <ResponsiveContainer width="100%" height="100%">
+        <LineChart
+          data={cities.slice(0, 5)}
+          margin={{ top: 20 }}
+          accessibilityLayer
+        >
+          <XAxis dataKey="City" padding={{ left: 30, right: 30 }} />
+          <YAxis tickCount={1} />
+          <Tooltip labelStyle={{ color: "black" }} />
+          <Legend />
+          <Line type="monotone" dataKey="Count" stroke="#3B82F6"></Line>
+        </LineChart>
+      </ResponsiveContainer>
+    </div>
   );
 }
