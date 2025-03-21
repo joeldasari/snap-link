edbf7ce67f84131834def2a263977d11a786b1ff
Joel D <joeldasari10@gmail.com>
2025-03-21 11:52:28 +0530

    Updated LinkPage

--- src/pages/LinkPage.jsx
+++ src/pages/LinkPage.jsx
@@ -156,7 +156,7 @@ const LinkPage = () => {
               )}
             </div>
           </div>
-          <div className="border p-0 rounded-lg xl:w-3/5 xl:p-8 flex flex-col gap-8">
+          <div className="border p-2 rounded-lg xl:w-3/5 xl:p-8 flex flex-col gap-8">
             {dataClicks && dataClicks.length ? (
               loadingClicks ? (
                 <p>Loading...</p>
